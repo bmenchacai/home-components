98949f5fac76b6d3d681d6dc0e4ec525de7c789c
bmenc <bmenc@mail.com>
2024-06-20 16:02:45 -0600

    tailwind init

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,39 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+
+  <div class="bg-white shadow-md p-4 rounded-md">
+    <input type="text" class="w-full px-3 py-2 rounded-md border focus:outline-none focus:ring focus:border-blue-300"
+      v-model="inputValue" @input="handleChange" />
+    <ul v-if="filteredOptions.length > 0" class="mt-1">
+      <li v-for="option in filteredOptions" :key="option" class="py-1 px-3 cursor-pointer hover:bg-gray-100">{{ option
+        }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputValue: '',
+      options: ['Apple', 'Banana', 'Cherry', 'Date'],
+    };
+  },
+  computed: {
+    filteredOptions() {
+      return this.options.filter(option =>
+        option.toLowerCase().includes(this.inputValue.toLowerCase())
+      );
+    },
+  },
+  methods: {
+    handleChange(event) {
+      this.inputValue = event.target.value;
+    },
+  },
+};
+</script>
 
 <style scoped>
 .logo {
@@ -21,9 +53,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
