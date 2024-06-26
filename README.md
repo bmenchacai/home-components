d1efe55b763aa8842e0ba0d8fb2b38e00b1742e6
bmenc <bmenc@mail.com>
2024-06-26 12:29:03 -0600

    chore Temp component created

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
-# Vue 3 + Vite
+# Vue 3 + Vite + Tailwind CSS
 
 This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
 
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Vue - Official](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (previously Volar) and disable Vetur
+
+
+## Git Syntax
+- 'build': Build related changes (eg: npm related/ adding external dependencies)
+- 'chore': A code change that external user won`t see (eg: change to .gitignore file)
+- 'feat': A new feature
+- 'fix': A bug fix
+- 'docs': Documentation related changes
+- 'ref': A code that neither fix bug nor adds a feature. (eg: You can use this
+                                                                when there is semantic changes
+                                                                like renaming a variable/
+                                                                function name)
+
+## Run
+- npm i
+- npm run dev
+
+
+## Help
+
+- https://flowbite.com/docs/components/
+- https://tailwindcomponents.com/cheatsheet/
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
 
-  <div class="bg-white shadow-md p-4 rounded-md">
-    <input type="text" class="w-full px-3 py-2 rounded-md border focus:outline-none focus:ring focus:border-blue-300"
-      v-model="inputValue" @input="handleChange" />
-    <ul v-if="filteredOptions.length > 0" class="mt-1">
-      <li v-for="option in filteredOptions" :key="option" class="py-1 px-3 cursor-pointer hover:bg-gray-100">{{ option
-        }}</li>
-    </ul>
-  </div>
+  <Main msg="Home Components" />
+
 </template>
 
 <script>
@@ -45,20 +30,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
