55dbc0e159be2a5169abf337a248e411dbf29d6b
bmenc <bmenc@mail.com>
2024-06-26 16:46:03 -0600

    build: shuffle tailwind component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,15 @@
 <template>
   <Temp />
+  <Shuffle />
 
   {{ msg }}
 
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import Temp from './Temp.vue'
+import { ref } from 'vue';
+import Temp from './Temp.vue';
+import Shuffle from './Shuffle.vue';
 
 defineProps({
   msg: String,
